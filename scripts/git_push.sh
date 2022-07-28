59cd4df82645cd036e4cda4723dc5ab3cc7c276d
Halil Ucel <hucelsoft@gmail.com>
2022-07-28 11:05:41 +0300

    test 2

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -1,9 +1,8 @@
 echo 'Commit için başlık gir'.
 read name
-
 git add .
-git commit -m "name"
+git commit -m "$name"
 echo 'Yayınlamak İçin Branch Gir'.
 read branches
-git branch branches
-git push -u origin branches
+git branch "$branches"
+git push -u origin "$branches"
